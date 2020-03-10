db48dcef0769e68f6240993f7732b42e30a29f5f
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-03-09 23:56:46 -0700

    Started work on proof_of_work helper method. Will add further complexity to sha256 input

--- blockchain.py
+++ blockchain.py
@@ -23,3 +23,11 @@ class Blockchain:
 
     def get_previous_block(self):
         return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+
+        while(check_proof is False):
+            #Create helper method later that will increase complexity of input for sha256 library
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2))
